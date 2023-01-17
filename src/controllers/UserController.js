af11cb5d0452c5606bca5112030c23825de4572e
luix gabriel <luisgasbriel@gmail.com>
2023-01-17 11:29:47 -0300

    Implementando o CRUD na API

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -10,6 +10,63 @@ class UserController {
       res.status(400).json({ errors: error.errors.map((err) => err.message) });
     }
   }
+
+  async getUsers(req, res) {
+    try {
+      const listUsers = await User.findAll();
+      return res.status(200).json(listUsers);
+    } catch (error) {
+      console.log(error);
+      return res.json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+      return res.status(200).json(user);
+    } catch (error) {
+      console.log(error);
+      return res.json(null);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        res.status(400).json({ errors: ['O usuário não existe na base de dados'] });
+      }
+      const user = await User.findByPk(id);
+      if (!user) {
+        res.status(400).json({ errors: ['O usuário não existe na base de dados'] });
+      }
+      const userAtualizado = await user.update(req.body);
+      return res.status(200).json(userAtualizado);
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json({ errors: error.errors.map((err) => err.message) });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        res.status(400).json({ errors: ['O usuário não existe na base de dados'] });
+      }
+      const user = await User.findByPk(id);
+      if (!user) {
+        res.status(400).json({ errors: ['O usuário não existe na base de dados'] });
+      }
+      await user.destroy();
+      return res.status(200).json({ msg: 'O usuario foi apagado com sucesso' });
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json({ errors: error.errors.map((err) => err.message) });
+    }
+  }
 }
 
 export default new UserController();
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,5 +4,9 @@ import UserController from '../controllers/UserController';
 const router = new Router();
 
 router.post('/', UserController.create);
+router.get('/listUsers', UserController.getUsers);
+router.get('/show/:id', UserController.show);
+router.put('/update/:id', UserController.update);
+router.delete('/delete/:id', UserController.delete);
 
 export default router;
