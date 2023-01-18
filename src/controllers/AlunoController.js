f60bf46740b0b6f062de4d80eb12a22ae61c03bc
luix gabriel <luisgasbriel@gmail.com>
2023-01-18 19:14:15 -0300

    Adicionando crud de alunos

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -5,6 +5,55 @@ class AlunoController {
     const aluno = await Aluno.findAll();
     return res.json(aluno);
   }
+
+  async create(req, res) {
+    try {
+      const novoAluno = await Aluno.create(req.body);
+      // const { id, nome, email } = novoUser;
+      return res.status(200).json(novoAluno);
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json({ errors: error.errors.map((err) => err.message) });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const aluno = await Aluno.findByPk(req.params.id);
+      return res.status(200).json(aluno);
+    } catch (error) {
+      console.log(error);
+      return res.json(null);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const aluno = await Aluno.findByPk(req.params.id);
+      if (!aluno) {
+        res.status(400).json({ errors: ['O aluno não existe na base de dados'] });
+      }
+      const alunoAtualizado = await aluno.update(req.body);
+      return res.status(200).json(alunoAtualizado);
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json({ errors: error.errors.map((err) => err.message) });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const aluno = await Aluno.findByPk(req.params.id);
+      if (!aluno) {
+        res.status(400).json({ errors: ['O aluno não existe na base de dados'] });
+      }
+      await aluno.destroy();
+      return res.status(200).json({ msg: 'O aluno foi apagado com sucesso' });
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json({ errors: error.errors.map((err) => err.message) });
+    }
+  }
 }
 
 export default new AlunoController();
--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 import AlunoController from '../controllers/AlunoController';
+import loginRequired from '../middlewares/validateToken';
 
 const router = new Router();
 
 router.get('/', AlunoController.index);
+router.post('/create', loginRequired, AlunoController.create);
+router.get('/show/:id', AlunoController.show);
+router.put('/update/:id', loginRequired, AlunoController.update);
+router.delete('/delete/:id', loginRequired, AlunoController.delete);
 
 export default router;
